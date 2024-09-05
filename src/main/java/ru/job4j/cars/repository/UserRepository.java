7d98a177027af7511839e567657a05f326841a38
Филипп Кучеров <filipp.kucherov@gazprombank.ru>
2024-09-05 09:03:49 +0700

    2. HQL [#6874] fix

--- src/main/java/ru/job4j/cars/repository/UserRepository.java
+++ src/main/java/ru/job4j/cars/repository/UserRepository.java
@@ -3,6 +3,7 @@ package ru.job4j.cars.repository;
 import lombok.AllArgsConstructor;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import ru.job4j.cars.model.User;
 
 import java.util.List;
@@ -18,38 +19,77 @@ public class UserRepository {
      * @return пользователь с id.
      */
     public User create(User user) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.save(user);
-        session.getTransaction().commit();
-        session.close();
+        Session session = null;
+        try {
+            session = sf.openSession();
+            session.beginTransaction();
+            session.save(user);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            if (session != null) {
+                session.getTransaction().rollback();
+            }
+            throw e;
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
         return user;
     }
 
     /**
-     * Обновить в базе пользователя.
+     * Обновить в базе пользователя через HQL.
      * @param user пользователь.
      */
     public void update(User user) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.update(user);
-        session.getTransaction().commit();
-        session.close();
+        Session session = null;
+        try {
+            session = sf.openSession();
+            session.beginTransaction();
+            String hql = "UPDATE User SET login = :login, password = :password WHERE id = :id";
+            Query query = session.createQuery(hql)
+                    .setParameter("login", user.getLogin())
+                    .setParameter("password", user.getPassword())
+                    .setParameter("id", user.getId());
+            query.executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            if (session != null) {
+                session.getTransaction().rollback();
+            }
+            throw e;
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     /**
-     * Удалить пользователя по id.
+     * Удалить пользователя по id через HQL.
      * @param userId ID
      */
     public void delete(int userId) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        User user = new User();
-        user.setId(userId);
-        session.delete(user);
-        session.getTransaction().commit();
-        session.close();
+        Session session = null;
+        try {
+            session = sf.openSession();
+            session.beginTransaction();
+            String hql = "DELETE FROM User WHERE id = :id";
+            Query query = session.createQuery(hql)
+                    .setParameter("id", userId);
+            query.executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            if (session != null) {
+                session.getTransaction().rollback();
+            }
+            throw e;
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     /**
@@ -57,16 +97,24 @@ public class UserRepository {
      * @return список пользователей.
      */
     public List<User> findAllOrderById() {
-        Session session = sf.openSession();
-        session.beginTransaction();
-
-        String hql = "FROM User ORDER BY id";
-        List<User> users = session.createQuery(hql, User.class)
-                .getResultList();
-
-        session.getTransaction().commit();
-        session.close();
-
+        Session session = null;
+        List<User> users;
+        try {
+            session = sf.openSession();
+            session.beginTransaction();
+            String hql = "FROM User ORDER BY id";
+            users = session.createQuery(hql, User.class).getResultList();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            if (session != null) {
+                session.getTransaction().rollback();
+            }
+            throw e;
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
         return users;
     }
 
@@ -75,12 +123,25 @@ public class UserRepository {
      * @return пользователь.
      */
     public Optional<User> findById(int userId) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        User result = session.get(User.class, userId);
-        session.getTransaction().commit();
-        session.close();
-        return Optional.ofNullable(result);
+        Session session = null;
+        Optional<User> result;
+        try {
+            session = sf.openSession();
+            session.beginTransaction();
+            User user = session.get(User.class, userId);
+            session.getTransaction().commit();
+            result = Optional.ofNullable(user);
+        } catch (Exception e) {
+            if (session != null) {
+                session.getTransaction().rollback();
+            }
+            throw e;
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return result;
     }
 
     /**
@@ -89,17 +150,26 @@ public class UserRepository {
      * @return список пользователей.
      */
     public List<User> findByLikeLogin(String key) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-
-        String hql = "FROM User WHERE login LIKE :key";
-        List<User> users = session.createQuery(hql, User.class)
-                .setParameter("key", "%" + key + "%")
-                .getResultList();
-
-        session.getTransaction().commit();
-        session.close();
-
+        Session session = null;
+        List<User> users;
+        try {
+            session = sf.openSession();
+            session.beginTransaction();
+            String hql = "FROM User WHERE login LIKE :key";
+            users = session.createQuery(hql, User.class)
+                    .setParameter("key", "%" + key + "%")
+                    .getResultList();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            if (session != null) {
+                session.getTransaction().rollback();
+            }
+            throw e;
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
         return users;
     }
 
@@ -109,14 +179,27 @@ public class UserRepository {
      * @return Optional or user.
      */
     public Optional<User> findByLogin(String login) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        String hql = "FROM User WHERE login = :login";
-        User result = session.createQuery(hql, User.class)
-                .setParameter("login", login)
-                .uniqueResult();
-        session.getTransaction().commit();
-        session.close();
-        return Optional.ofNullable(result);
+        Session session = null;
+        Optional<User> result;
+        try {
+            session = sf.openSession();
+            session.beginTransaction();
+            String hql = "FROM User WHERE login = :login";
+            User user = session.createQuery(hql, User.class)
+                    .setParameter("login", login)
+                    .uniqueResult();
+            session.getTransaction().commit();
+            result = Optional.ofNullable(user);
+        } catch (Exception e) {
+            if (session != null) {
+                session.getTransaction().rollback();
+            }
+            throw e;
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return result;
     }
 }
